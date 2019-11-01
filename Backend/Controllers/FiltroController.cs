ad1dcd06e3c4438fa93d6154b465f8ee6c4007d3
eichild <44980461+eichild@users.noreply.github.com>
2019-11-01 14:44:48 -0300

    Adicionado mensagem de erro

--- Backend/Controllers/FiltroController.cs
+++ Backend/Controllers/FiltroController.cs
@@ -4,25 +4,31 @@ using Backend.Domains;
 using Backend.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Backend.Controllers
-{
-    [Route("api/[controller]")]
+namespace Backend.Controllers {
+    [Route ("api/[controller]")]
     [ApiController]
-    public class FiltroController : ControllerBase
-    {
-        [HttpGet("FiltrarPalavra")]
-        public ActionResult<List<Oferta>> GetFiltrar(FiltroViewModel filtro){
-            using(bddatempoContext _contexto = new bddatempoContext()){
-                List<Oferta> ofertas = _contexto.Oferta.Where(o => o.NomeOferta.Contains(filtro.Palavra)).ToList();
-
+    public class FiltroController : ControllerBase {
+        [HttpGet ("FiltrarPalavra")]
+        public ActionResult<List<Oferta>> GetFiltrar (FiltroViewModel filtro) {
+            using (bddatempoContext _contexto = new bddatempoContext ()) {
+                List<Oferta> ofertas = _contexto.Oferta.Where (o => o.NomeOferta.Contains (filtro.Palavra)).ToList ();
+                if (ofertas == null) {
+                    return NotFound (
+                        new {
+                            Mensagem = "Produto não encontrado",
+                                Erro = true
+                        });
+                }
                 return ofertas;
             }
         }
-        [HttpGet("Ordenar")]
-        public ActionResult<List<Oferta>> GetOrdernar(){
+
+        [HttpGet ("Ordenar")]
+        public ActionResult<List<Oferta>> GetOrdernar () {
             List<Oferta> ofertas;
-            using(bddatempoContext _contexto = new bddatempoContext()){
-                ofertas = _contexto.Oferta.OrderBy(o => o.Preco).ToList();
+            using (bddatempoContext _contexto = new bddatempoContext ()) {
+                ofertas = _contexto.Oferta.OrderBy (o => o.Preco).ToList ();
+
             }
             return ofertas;
         }
