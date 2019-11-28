8f722ac60a86e5b4450668d0ed5e7ebfc2da92ee
FIC <FIC@info.local>
2019-11-28 09:15:30 -0300

    Página Sobre Nós pronta

--- Frontend/src/pages/Sobrenos/sobrenos.js
+++ Frontend/src/pages/Sobrenos/sobrenos.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
+import gastos from '../../assets/imagens/piggy-bank.png';
+import desperdicio from '../../assets/imagens/plant.png';
+import pessoas from '../../assets/imagens/hands.png';
+import bruna from '../../assets/imagens/bruna.jpg';
+import lucas from '../../assets/imagens/lucas.jpg';
+import paty from '../../assets/imagens/paty.jpg';
+import nilson from '../../assets/imagens/nilson.jpg';
+import yasmin from '../../assets/imagens/yasmin.jpg';
+import rubao from '../../assets/imagens/rubao.jpg';
+
 
 
 class Sobrenos extends Component {
@@ -14,37 +24,60 @@ class Sobrenos extends Component {
                     <section className="sn_topo">
                         <div className="sn_texto">
                             <h2>SOBRE NÓS</h2>
-                            <hr/> 
+                            <hr />
                             <p>O projeto Datempo foi criado durante o Code XP 2019 no Senai, em uma parceria com a empresa Linx,
                                 com o intuito de gerar um impacto social. O marketplace funciona como um agente de solução, conectando empresas e clientes, com a finalidade de diminuir o desperdício, reduzir gastos e
-                                ajudar pessoas. Você encontrará em nossa plataforma ofertas de produtos como: alimentos que estão perto do vencimento e vestimentas que possuem pouca circulação no estoque.<br/>
+                                ajudar pessoas. Você encontrará em nossa plataforma ofertas de produtos como: alimentos que estão perto do vencimento e vestimentas que possuem pouca circulação no estoque.<br />
                                 Nunca é tarde, sempre DATEMPO!</p>
                         </div>
                     </section>
+                    <section className="objetivos">
+                        <h2>OBJETIVOS</h2>
+                        <hr />
+                        <div className="sn_section2">
+                            <div className="quadro1">
+                                <img src={gastos} alt="Imagem de um cofrinho representando o ideal de reduzir gastos" />
+                                <p>REDUZIR GASTOS</p>
+                            </div>
+                            <div className="quadro1">
+                                <img src={desperdicio} alt="Imagem de duas mãos segurando uma muda de planta para representar a diminuição do desperdício de alimento" />
+                                <p>DIMINUIR O DESPERDÍCIO</p>
+                            </div>
+                            <div className="quadro1">
+                                <img src= {pessoas} alt="Imagem de duas mãos com um coraçao no meio, representando solidariedade" />
+                                <p>AJUDAR PESSOAS</p>
+                            </div>
+                        </div>
+                    </section>
+
+                    <div className="sn_h2_dev">
+                        <h2>EQUIPE</h2>
+                        <hr />
+                    </div>
                     <section className="sn_dev">
                         <div className="dev">
-                            <img src="Imagens/bruna.jpg" alt="foto de perfil da desenvolvedora Bruna Ribeiro" />
-                            <p>Bruna Ribeiro<br/>Desenvolvedora Back-end</p>
+                            <img src={bruna} alt="foto de perfil da desenvolvedora Bruna Ribeiro" />
+                            <p>Bruna Ribeiro<br />Desenvolvedora Back-end</p>
                         </div>
                         <div className="dev">
-                            <img src="Imagens/lucas.jpg" alt="foto de perfil do desenvolvedor Lucas Gregorio" />
-                            <p>Lucas Gregorio <br/>Desenvolvedor Front-end</p>
+                            <img src= {lucas} alt="foto de perfil do desenvolvedor Lucas Gregorio" />
+                            <p>Lucas Gregorio <br />Desenvolvedor Front-end</p>
                         </div>
                         <div className="dev">
-                            <img src="Imagens/nilson.jpg" alt="foto de perfil do desenvolvedor Nilson Ledres" />
-                            <p>Nilson Ledres<br/>Designer</p>
+                            <img src= {nilson} alt="foto de perfil do desenvolvedor Nilson Ledres" />
+                            <p>Nilson Ledres<br />Designer</p>
                         </div>
                         <div className="dev">
-                            <img src="Imagens/paty.jpg" alt="foto de perfil da desenvolvedora Patrícia Haddad" />
-                            <p>Patrícia Haddad<br/>Desenvolvedora Front-end</p>
+                            <img src= {paty} alt="foto de perfil da desenvolvedora Patrícia Haddad" />
+                            <p>Patrícia Haddad<br />Desenvolvedora Front-end</p>
                         </div>
                         <div className="dev">
-                            <img src="Imagens/rubao.jpg" alt="foto de perfil do desenvolvedor Ruben de Castro" />
-                            <p>Ruben de Castro<br/>Desenvolvedor Back-end</p>
+                            <img src= {rubao} alt="foto de perfil do desenvolvedor Ruben de Castro" />
+                            <p>Ruben de Castro<br />Desenvolvedor Back-end</p>
                         </div>
                         <div className="dev">
-                            <img src="Imagens/yasmin.jpg" alt="foto de perfil do desenvolvedora Yasmin Ribeiro" />
-                            <p>Yasmin Ribeiro<br/>Desenvolvedora Front-end</p>
+                            <img src= {yasmin} alt="foto de perfil do desenvolvedora Yasmin Ribeiro" />
+                            <p>Yasmin Ribeiro<br />Desenvolvedora Front-end</p>
                         </div>
                     </section>
                 </main>
