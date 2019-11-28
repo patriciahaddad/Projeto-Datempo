89abec78227926813d7927458cab22c34abfa029
eichild <44980461+eichild@users.noreply.github.com>
2019-11-28 10:49:05 -0300

    Desabilitado http redirect e cors habilitado

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -4,8 +4,10 @@ import './assets/css/estilo.css';
 import App from '../src/pages/Home/App';
 import * as serviceWorker from './serviceWorker';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+
 import Sobrenos from './pages/Sobrenos/sobrenos';
 import Mostruario from './pages/Mostruario/mostruario';
+import Carrinho from './pages/Carrinho/carrinho';
 
 
 //Realizamos a criação das rotas
@@ -16,6 +18,7 @@ const Rotas = (
                 <Route exact path="/" component={App} />
                 <Route path="/sobrenos" component={Sobrenos} />
                 <Route path="/mostruario" component={Mostruario} />
+                <Route path= "/carrinho" component={Carrinho}/>
             </Switch>
         </div>
     </Router>
