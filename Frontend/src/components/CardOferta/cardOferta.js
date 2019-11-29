b2dc722b7b5c76e1bc31865bb6e581b8683a2e77
eichild <44980461+eichild@users.noreply.github.com>
2019-11-29 15:32:10 -0300

    Adicionado get de quantidade de produtos na pagina/Terminar o get das informaçççoes do produto

--- Frontend/src/components/CardOferta/cardOferta.js
+++ Frontend/src/components/CardOferta/cardOferta.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBCol } from 'mdbreact';
+import Relogio from '../../assets/imagens/alarm-clock.png';
+import Produto from '../../assets/imagens/arroz.png';
+import api from '../../services/api';
 
 
 class CardOferta extends Component {
@@ -9,24 +12,68 @@ class CardOferta extends Component {
             listaOferta: []
         }
     }
+    componentDidMount() {
+        // console.log(this.state.listaOferta);
+        this.getOferta();
+    }
+
+    getOferta = () => {
+        api.get('/oferta')
+            .then(response => {
+                if (response.status === 200) {
+                    this.setState({ listaOferta: response.data });
+                }
+            })
+    }
 
 
     render() {
         return (
-            <MDBCol>
-                <MDBCard style={{ width: "22rem" }}>
-                    <MDBCardImage className="img-fluid" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" waves />
-                    <MDBCardBody>
-                        <MDBCardTitle>Card title</MDBCardTitle>
-                        <MDBCardText>
-                            Some quick example text to build on the card title and make
-                            up the bulk of the card&apos;s content.
-                        </MDBCardText>
-                        <MDBBtn href="#">MDBBtn</MDBBtn>
-                    </MDBCardBody>
-                </MDBCard>
-            </MDBCol>);
+            <div className="container_ofertas">
+                {
+                    this.state.listaOferta.map(function (o) {
+                        return (
+                            <div className="card_oferta">
+                                <div className="caixa_imagem">
+                                    <img className="imgproduto" src={Produto}
+                                        alt="Pacote de Arroz de 5kg da marca Tio João" />
+                                </div>
+                                <div className="descricao_oferta">
+                                    <div className="titulo_produto">
+                                        <p className="titulo descricao">{o.nomeOferta}</p>
+                                    </div>
+                                    <div className="descricao_produto">
+                                        <div className="descricao_pequena">
+                                            <p className="titulo_descricao">de R$ 8,00</p>
+                                            <p className="titulo_preco">Por</p>
+                                            <p className="preco_descricao">R$ 5,00</p>
+                                        </div>
+
+                                        <div className="descricao_pequena_logo">
+                                            <p className="titulo_descricao_logo">DATEMPO</p>
+                                            <div className="validade_mostruario">
+                                                <img src={Relogio} alt="Alarme" />
+                                                <p className="descricao"> Faltam: 10 dias!</p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                                <div className="botao_reservar">
+                                    <a href="#" className="btn_reservar">RESERVAR</a>
+                                </div>
+
+                            </div>
+                        )
+
+                    })
+
+                }
+            </div>
+        )
     }
 
 }
+
 export default CardOferta;
+
--- Frontend/src/components/Footer/Footer.js
+++ Frontend/src/components/Footer/Footer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import '../../assets/css/footer.css'
 
 class Footer extends Component {
 
