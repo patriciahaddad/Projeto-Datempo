11191795e0673d0c2778f227b70eeeab76394bef
eichild <44980461+eichild@users.noreply.github.com>
2019-11-29 14:29:55 -0300

    Reset em commit

---
+++ Frontend/src/pages/Ajuda/ajuda.js
@@ -0,0 +1,108 @@
+import React, { Component } from 'react';
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+
+
+
+
+class ajuda extends Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <main>
+                    <div className="cont_branco" id="accordionExample">
+                                <div className="card_aj">
+                                    <div className="card-header" id="headingOne">
+                                        <h2 className="mb-0">
+                                            <button className="btn_aj " type="button" data-toggle="collapse" data-target="#collapseOne"
+                                                aria-expanded="true" aria-controls="collapseOne">
+                                                Como funciona o Datempo<hr />
+                                            </button>
+                                        </h2>
+                                    </div>
+
+                                    <div id="collapseOne" className="collapse show" aria-labelledby="headingOne"
+                                        data-parent="#accordionExample">
+                                        <div className="card-body">
+                                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
+                                            3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt
+                                            laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin
+                                            coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes
+                                            anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings
+                                            occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard
+                                            of them accusamus labore sustainable VHS.
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="card_aj">
+                                    <div className="card-header" id="headingTwo">
+                                        <h2 className="mb-0">
+                                            <button className="btn_aj" type="button" data-toggle="collapse"
+                                                data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                                Como anunciar os produtos<hr />
+                                            </button>
+                                        </h2>
+                                    </div>
+                                    <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                                        <div className="card-body">
+                                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
+                                            3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt
+                                            laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin
+                                            coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes
+                                            anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings
+                                            occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard
+                                            of them accusamus labore sustainable VHS.
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="card_aj">
+                                    <div className="card-header" id="headingThree">
+                                        <h2 className="mb-0">
+                                            <button className="btn_aj" type="button" data-toggle="collapse"
+                                                data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                                Opções de retirada<hr />
+                                            </button>
+                                        </h2>
+                                    </div>
+                                    <div id="collapseThree" className="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                                        <div className="card-body">
+                                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
+                                            3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt
+                                            laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin
+                                            coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes
+                                            anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings
+                                            occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard
+                                            of them accusamus labore sustainable VHS.
+                                </div>
+                                    </div>
+                                </div>
+                        <div className="card_aj">
+                            <div className="card-header" id="headingfor">
+                                <h2 className="mb-0">
+                                    <button className="btn_aj" type="button" data-toggle="collapse"
+                                        data-target="#collapsefor" aria-expanded="false" aria-controls="collapsefor">
+                                        Como reservar no site<hr />
+                                    </button>
+                                </h2>
+                            </div>
+                            <div id="collapsefor" className="collapse" aria-labelledby="headingfor" data-parent="#accordionExample">
+                                <div className="card-body">
+                                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
+                                    3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt
+                                    laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin
+                                    coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes
+                                    anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings
+                                    occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard
+                                    of them accusamus labore sustainable VHS.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+                <Footer />
+            </div>
+        );
+    }
+}
+export default ajuda;
---
+++ Frontend/src/pages/MinhasOfertas/minhasofertas.js
@@ -0,0 +1,312 @@
+import React, { Component } from 'react';
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+import ImagemCard from '../../assets/imagens/arroz.png';
+import Relogio from '../../assets/imagens/alarm-clock.png';
+
+class Minhasofertas extends Component {
+    
+    constructor(){
+        super()
+        
+        
+    }
+    render() {
+        return (
+            <div>
+                <Header />
+                <main>
+                    <div className="cont_branco">
+                    <div className="ofertas_cadastradas">
+                        <h2>MINHAS OFERTAS</h2>
+                        <hr />
+                            <div className="filtro">
+                            <div className="filtros">
+                                <a href="cadastroOfertas.html" className="btn_cria_Oferta">cadastrar ofertas</a>
+                                <a href="#" className="btn-filtro">Selecione por:</a>
+                            </div>
+                            <p className="qnt_ofertas">Mostrando 1 - 12 de 30 resultados</p>
+
+                            </div>
+
+                            <div className="container_card">
+                                <div className="card_oferta">
+                                <div className="caixa_imagem">
+                                    <img className="imgproduto" src={ImagemCard}
+                                    alt="Pacote de Arroz de 5kg da marca Tio João" />
+                                </div>
+                            <div className="descricao_oferta">
+                                <div className="titulo_produto">
+                                    <p className="titulo descricao">Arroz Tio João - 5kg</p>
+                                </div>
+                                <div className="descricao_produto">
+                                    <div className="descricao_pequena">
+                                        <p className="titulo_descricao">de R$ 8,00</p>
+                                        <p className="titulo_preco">Por</p>
+                                        <p className="preco_descricao">R$ 5,00</p>
+                                    </div>
+
+                                    <div className="descricao_pequena_logo">
+                                        <p className="titulo_descricao_logo">DATEMPO</p>
+                                        <div className="validade_mostruario">
+                                            <img src={Relogio} alt="Alarme" />
+                                            <p className="descricao"> Faltam: 10 dias!</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                                    <div className="botoes_oferta">
+                                        <a href="#" className="btn_edita_oferta">EDITAR</a>
+                                        <a href="#" className="btn_reserva_oferta">RESERVAS</a>
+                                    </div>
+                                </div>
+
+                                <div className="card_oferta">
+                                <div className="caixa_imagem">
+                                    <img className="imgproduto" src={ImagemCard}
+                                    alt="Pacote de Arroz de 5kg da marca Tio João" />
+                                </div>
+                            <div className="descricao_oferta">
+                                <div className="titulo_produto">
+                                    <p className="titulo descricao">Arroz Tio João - 5kg</p>
+                                </div>
+                                <div className="descricao_produto">
+                                    <div className="descricao_pequena">
+                                        <p className="titulo_descricao">de R$ 8,00</p>
+                                        <p className="titulo_preco">Por</p>
+                                        <p className="preco_descricao">R$ 5,00</p>
+                                    </div>
+
+                                    <div className="descricao_pequena_logo">
+                                        <p className="titulo_descricao_logo">DATEMPO</p>
+                                        <div className="validade_mostruario">
+                                            <img src={Relogio} alt="Alarme" />
+                                            <p className="descricao"> Faltam: 10 dias!</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                                    <div className="botoes_oferta">
+                                        <a href="#" className="btn_edita_oferta">EDITAR</a>
+                                        <a href="#" className="btn_reserva_oferta">RESERVAS</a>
+                                    </div>
+                                </div>
+
+                                <div className="card_oferta">
+                                <div className="caixa_imagem">
+                                    <img className="imgproduto" src={ImagemCard}
+                                    alt="Pacote de Arroz de 5kg da marca Tio João" />
+                                </div>
+                            <div className="descricao_oferta">
+                                <div className="titulo_produto">
+                                    <p className="titulo descricao">Arroz Tio João - 5kg</p>
+                                </div>
+                                <div className="descricao_produto">
+                                    <div className="descricao_pequena">
+                                        <p className="titulo_descricao">de R$ 8,00</p>
+                                        <p className="titulo_preco">Por</p>
+                                        <p className="preco_descricao">R$ 5,00</p>
+                                    </div>
+
+                                    <div className="descricao_pequena_logo">
+                                        <p className="titulo_descricao_logo">DATEMPO</p>
+                                        <div className="validade_mostruario">
+                                            <img src={Relogio} alt="Alarme" />
+                                            <p className="descricao"> Faltam: 10 dias!</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                                    <div className="botoes_oferta">
+                                        <a href="#" className="btn_edita_oferta">EDITAR</a>
+                                        <a href="#" className="btn_reserva_oferta">RESERVAS</a>
+                                    </div>
+                                </div>
+
+                                <div className="card_oferta">
+                                <div className="caixa_imagem">
+                                    <img className="imgproduto" src={ImagemCard}
+                                    alt="Pacote de Arroz de 5kg da marca Tio João" />
+                                </div>
+                            <div className="descricao_oferta">
+                                <div className="titulo_produto">
+                                    <p className="titulo descricao">Arroz Tio João - 5kg</p>
+                                </div>
+                                <div className="descricao_produto">
+                                    <div className="descricao_pequena">
+                                        <p className="titulo_descricao">de R$ 8,00</p>
+                                        <p className="titulo_preco">Por</p>
+                                        <p className="preco_descricao">R$ 5,00</p>
+                                    </div>
+
+                                    <div className="descricao_pequena_logo">
+                                        <p className="titulo_descricao_logo">DATEMPO</p>
+                                        <div className="validade_mostruario">
+                                            <img src={Relogio} alt="Alarme" />
+                                            <p className="descricao"> Faltam: 10 dias!</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                                    <div className="botoes_oferta">
+                                        <a href="#" className="btn_edita_oferta">EDITAR</a>
+                                        <a href="#" className="btn_reserva_oferta">RESERVAS</a>
+                                    </div>
+                                </div>
+
+                                <div className="card_oferta">
+                                <div className="caixa_imagem">
+                                    <img className="imgproduto" src={ImagemCard}
+                                    alt="Pacote de Arroz de 5kg da marca Tio João" />
+                                </div>
+                            <div className="descricao_oferta">
+                                <div className="titulo_produto">
+                                    <p className="titulo descricao">Arroz Tio João - 5kg</p>
+                                </div>
+                                <div className="descricao_produto">
+                                    <div className="descricao_pequena">
+                                        <p className="titulo_descricao">de R$ 8,00</p>
+                                        <p className="titulo_preco">Por</p>
+                                        <p className="preco_descricao">R$ 5,00</p>
+                                    </div>
+
+                                    <div className="descricao_pequena_logo">
+                                        <p className="titulo_descricao_logo">DATEMPO</p>
+                                        <div className="validade_mostruario">
+                                            <img src={Relogio} alt="Alarme" />
+                                            <p className="descricao"> Faltam: 10 dias!</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                                    <div className="botoes_oferta">
+                                        <a href="#" className="btn_edita_oferta">EDITAR</a>
+                                        <a href="#" className="btn_reserva_oferta">RESERVAS</a>
+                                    </div>
+                                </div>
+
+                                <div className="card_oferta">
+                                <div className="caixa_imagem">
+                                    <img className="imgproduto" src={ImagemCard}
+                                    alt="Pacote de Arroz de 5kg da marca Tio João" />
+                                </div>
+                            <div className="descricao_oferta">
+                                <div className="titulo_produto">
+                                    <p className="titulo descricao">Arroz Tio João - 5kg</p>
+                                </div>
+                                <div className="descricao_produto">
+                                    <div className="descricao_pequena">
+                                        <p className="titulo_descricao">de R$ 8,00</p>
+                                        <p className="titulo_preco">Por</p>
+                                        <p className="preco_descricao">R$ 5,00</p>
+                                    </div>
+
+                                    <div className="descricao_pequena_logo">
+                                        <p className="titulo_descricao_logo">DATEMPO</p>
+                                        <div className="validade_mostruario">
+                                            <img src={Relogio} alt="Alarme" />
+                                            <p className="descricao"> Faltam: 10 dias!</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                                    <div className="botoes_oferta">
+                                        <a href="#" className="btn_edita_oferta">EDITAR</a>
+                                        <a href="#" className="btn_reserva_oferta">RESERVAS</a>
+                                    </div>
+                                </div>
+
+                                <div className="card_oferta">
+                                <div className="caixa_imagem">
+                                    <img className="imgproduto" src={ImagemCard}
+                                    alt="Pacote de Arroz de 5kg da marca Tio João" />
+                                </div>
+                            <div className="descricao_oferta">
+                                <div className="titulo_produto">
+                                    <p className="titulo descricao">Arroz Tio João - 5kg</p>
+                                </div>
+                                <div className="descricao_produto">
+                                    <div className="descricao_pequena">
+                                        <p className="titulo_descricao">de R$ 8,00</p>
+                                        <p className="titulo_preco">Por</p>
+                                        <p className="preco_descricao">R$ 5,00</p>
+                                    </div>
+
+                                    <div className="descricao_pequena_logo">
+                                        <p className="titulo_descricao_logo">DATEMPO</p>
+                                        <div className="validade_mostruario">
+                                            <img src={Relogio} alt="Alarme" />
+                                            <p className="descricao"> Faltam: 10 dias!</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                                    <div className="botoes_oferta">
+                                        <a href="#" className="btn_edita_oferta">EDITAR</a>
+                                        <a href="#" className="btn_reserva_oferta">RESERVAS</a>
+                                    </div>
+                                </div>  
+                                   
+                                <div className="card_oferta">
+                                <div className="caixa_imagem">
+                                    <img className="imgproduto" src={ImagemCard}
+                                    alt="Pacote de Arroz de 5kg da marca Tio João" />
+                                </div>
+                            <div className="descricao_oferta">
+                                <div className="titulo_produto">
+                                    <p className="titulo descricao">Arroz Tio João - 5kg</p>
+                                </div>
+                                <div className="descricao_produto">
+                                    <div className="descricao_pequena">
+                                        <p className="titulo_descricao">de R$ 8,00</p>
+                                        <p className="titulo_preco">Por</p>
+                                        <p className="preco_descricao">R$ 5,00</p>
+                                    </div>
+
+                                    <div className="descricao_pequena_logo">
+                                        <p className="titulo_descricao_logo">DATEMPO</p>
+                                        <div className="validade_mostruario">
+                                            <img src={Relogio} alt="Alarme" />
+                                            <p className="descricao"> Faltam: 10 dias!</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                                    <div className="botoes_oferta">
+                                        <a href="#" className="btn_edita_oferta">EDITAR</a>
+                                        <a href="#" className="btn_reserva_oferta">RESERVAS</a>
+                                    </div>
+                                </div>  
+                            </div>
+          
+                    </div>
+                        <div className="paginacao_ofertas">
+                            <ul className="lista_paginacao">
+                                <a href="#" clas="lk_paginacao">
+                                    <li>
+                                    </li>  </a> <a href="#">
+                                    <li>1</li>
+                                    </a>
+                                    <a href="#">
+                                        <li>2</li>
+                                    </a>
+                                    <a href="#">
+                                        <li>3</li>
+                                    </a>
+                                    <a href="#">
+                                    <li>...</li>
+                                    </a>
+                                    <a href="#">
+                                        <li> > </li>
+                                    </a>
+                            </ul>
+                        </div> 
+                    </div>
+                </main>
+                <Footer />
+            </div>
+        );
+    }
+}
+
+export default Minhasofertas;
