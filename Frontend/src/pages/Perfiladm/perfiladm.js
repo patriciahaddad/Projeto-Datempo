d9e5448a6528994e99d807bec28defa4abc9313d
eichild <44980461+eichild@users.noreply.github.com>
2019-12-02 09:48:37 -0300

    Pull nos arquivos

--- Frontend/src/pages/Perfiladm/perfiladm.js
+++ Frontend/src/pages/Perfiladm/perfiladm.js
@@ -2,9 +2,184 @@ import React, { Component } from 'react';
 import Header from '../../components/Header/Header.js';
 import Footer from '../../components/Footer/Footer.js';
 import ImagemPerfil from '../../assets/imagens/avatar.png';
+import api from './../../services/api';
+
+import {
+    MDBContainer,
+    MDBBtn,
+    MDBModal,
+    MDBModalBody,
+    MDBModalHeader,
+    MDBModalFooter,
+    MDBDropdown,
+    MDBDropdownToggle,
+    MDBDropdownMenu,
+    MDBDropdownItem,
+    MDBTable,
+    MDBTableBody,
+    MDBTableHead,
+    MDBAlert
+} from 'mdbreact';
+
 
 
 class Perfiladm extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+
+            listaCategorias: [],
+            listaOfertas: [],
+            listaProdutos: [],
+            listaUsuarios: [],
+
+            postCategoria: {
+                nomeCategoria: "",
+            },
+
+            postProduto: {
+                nomeProduto: "",
+                idCategoria: "",
+            },
+
+            putCategoria: {
+                idCategoria: "",
+                nomeCategoria: "",
+            },
+
+            putProduto: {
+                idProduto: "",
+                nomeProduto: "",
+                idCategoria: "",
+            },
+
+            erroMsg: "",
+            sucessMsg: "",
+            modal1: false,
+            modal2: false,
+            modal3: false
+        }
+    }
+
+    toggle = nr => () => {
+        let modalNumber = 'modal' + nr
+        this.setState({
+            [modalNumber]: !this.state[modalNumber]
+        });
+    }
+
+    componentDidMount() {
+        this.getCategorias();
+        this.getProdutos();
+    }
+
+    openModalCategoria = (c) => {
+        this.toggle();
+
+        this.setState({ getCategoria: c });
+        console.log("GET", this.state.getCategoria);
+    }
+
+    openModalEditarCategoria = (c) => {
+        this.toggle();
+
+        this.setState({ putCategoria: c });
+        console.log("PUT", this.state.putCategoria);
+    }
+
+    //#region GET
+    getCategorias = () => {
+        api.get('/categoria')
+            .then(response => {
+                if (response.status === 200) {
+                    this.setState({ listaCategorias: response.data })
+                }
+            })
+    }
+
+    getProdutos = () => {
+        api.get('/produto')
+            .then(response => {
+                if (response.status === 200) {
+                    this.setState({ listaProdutos: response.data })
+                }
+            })
+    }
+
+    getUsuarios = () => {
+        api.get('/usuario')
+            .then(response => {
+                if (response.status === 200) {
+                    this.setState({ listaUsuarios: response.data })
+                }
+            })
+    }
+
+    //#endregion
+
+    //#region PUT
+
+    putSetState = (input) => {
+        this.setState({
+            putEvento: {
+                ...this.state.putCategoria, [input.target.name]: input.target.value
+            }
+        })
+    }
+
+    putCategoria = (event) => {
+        event.preventDefault();
+        let categoria_id = this.state.putCategoria.idCategoria;
+        let categoria_alterado = this.state.putCategoria;
+
+        api.put('/categoria/' + categoria_id, categoria_alterado)
+            .then(() => {
+                this.setState({ sucessMsg: "Categoria alterada com sucesso!" });
+            })
+            .catch(error => {
+                console.log(error);
+                this.setState({ erroMsg: "Falha ao alterar o Categoria!" });
+            })
+
+        this.toggle();
+
+        setTimeout(() => {
+            this.getEventos();
+        }, 1500);
+    }
+
+    //#endregion
+
+    //#region POSTs
+    postSetState = (input) => {
+        this.setState({
+            postEvento: {
+                ...this.state.postCategoria, [input.target.name]: input.target.value
+            }
+        })
+    }
+
+    postCategoria = (c) => {
+
+        c.preventDefault();
+
+        api.post('/categoria', this.state.postCategoria)
+            .then(response => {
+                console.log(response);
+                this.setState({ sucessMsg: "Categoria cadastrada com sucesso!" });
+            })
+            .catch(error => {
+                console.log(error);
+                this.setState({ erroMsg: "Não foi possível cadastrar categoria!" });
+            })
+
+        setTimeout(() => {
+            this.getCategorias();
+        }, 1500);
+    }
+    //#endregion
+
     render() {
         return (
             <div>
@@ -14,10 +189,43 @@ class Perfiladm extends Component {
                         <section className="cont_branco">
                             <div className="organizacao_adm">
                                 <div className="adm_configs_esq">
-                                    <button className="adm_btn_01" type="submit">CATEGORIA</button>
-                                    <button className="adm_btn_01" type="submit">OFERTA</button>
-                                    <button className="adm_btn_01" type="submit">PRODUTO</button>
-                                    <button className="adm_btn_01" type="submit">USUÁRIO</button>
+                                    <MDBDropdown dropright>
+                                        <MDBDropdownToggle caret className="adm_btn_01" color="purple darken-3">
+                                            Categoria
+                                        </MDBDropdownToggle>
+                                        <MDBDropdownMenu basic>
+                                            <MDBDropdownItem onClick={this.toggle(2)}>Cadastrar nova</MDBDropdownItem>
+                                            <MDBDropdownItem onClick={this.toggle(1)}>Visualizar Categorias</MDBDropdownItem>
+                                        </MDBDropdownMenu>
+                                    </MDBDropdown>
+                                    <MDBDropdown dropright>
+                                        <MDBDropdownToggle caret className="adm_btn_01" color="purple darken-3">
+                                            Produto
+                                        </MDBDropdownToggle>
+                                        <MDBDropdownMenu basic>
+                                            <MDBDropdownItem>Cadastrar nova</MDBDropdownItem>
+                                            <MDBDropdownItem onClick={this.toggle(3)}>Visualizar Produtos</MDBDropdownItem>
+                                        </MDBDropdownMenu>
+                                    </MDBDropdown>
+                                    <MDBDropdown dropright>
+                                        <MDBDropdownToggle caret className="adm_btn_01" color="purple darken-3">
+                                            Oferta
+                                        </MDBDropdownToggle>
+                                        <MDBDropdownMenu basic>
+                                            <MDBDropdownItem>Cadastrar nova</MDBDropdownItem>
+                                            <MDBDropdownItem>Visualizar Ofertas</MDBDropdownItem>
+                                        </MDBDropdownMenu>
+                                    </MDBDropdown>
+                                    <MDBDropdown dropright>
+                                        <MDBDropdownToggle caret className="adm_btn_01" color="purple darken-3">
+                                            Usuário
+                                        </MDBDropdownToggle>
+                                        <MDBDropdownMenu basic>
+                                            <MDBDropdownItem>Cadastrar novo</MDBDropdownItem>
+                                            <MDBDropdownItem>Visualizar Usuários</MDBDropdownItem>
+                                        </MDBDropdownMenu>
+                                    </MDBDropdown>
+
                                 </div>
 
                                 <div className="adm_configs_dir">
@@ -45,13 +253,135 @@ class Perfiladm extends Component {
                             </div>
                         </section>
                     </div>
+
+                    {/* Container para visualizar categorias cadastradas */}
+                    <MDBContainer>
+                        <MDBModal isOpen={this.state.modal1} toggle={this.toggle(1)} centered>
+                            <MDBModalHeader toggle={this.toggle(1)}>Categorias</MDBModalHeader>
+                            <MDBModalBody>
+                                <MDBTable>
+                                    <MDBTableHead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Categoria</th>
+                                            <th>Ações</th>
+                                        </tr>
+                                    </MDBTableHead>
+                                    <MDBTableBody>
+                                        {
+                                            this.state.listaCategorias.map(
+                                                function (c) {
+                                                    return (
+                                                        <tr key={c.idCategoria}>
+                                                            <td>{c.idCategoria}</td>
+                                                            <td>{c.nomeCategoria}</td>
+                                                            <td>
+                                                                <MDBBtn color="primary" size="sm" onClick={() => this.openModalEditarCategoria(c)}>
+                                                                    Editar
+                                                                </MDBBtn>
+                                                                <MDBBtn color="danger" size="sm" onClick={() => this.deleteEvento(c.idCategoria)}>
+                                                                    Excluir
+                                                                </MDBBtn>
+                                                            </td>
+                                                        </tr>
+                                                    )
+                                                }.bind(this)
+                                            )
+                                        }
+                                    </MDBTableBody>
+                                </MDBTable>
+                            </MDBModalBody>
+                            <MDBModalFooter>
+                                <MDBBtn color="secondary" onClick={this.toggle(1)}>Fechar</MDBBtn>
+                                <MDBBtn color="primary">Salvar</MDBBtn>
+                            </MDBModalFooter>
+                        </MDBModal>
+                    </MDBContainer>
+
+                    {/* Container para visualizar produtos cadastrados */}
+                    <MDBContainer>
+                        <MDBModal isOpen={this.state.modal3} toggle={this.toggle(3)} centered>
+                            <MDBModalHeader toggle={this.toggle(3)}>Categorias</MDBModalHeader>
+                            <MDBModalBody>
+                                <MDBTable>
+                                    <MDBTableHead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Produtos</th>
+                                            <th>Categoria</th>
+                                            <th>Ações</th>
+                                        </tr>
+                                    </MDBTableHead>
+                                    <MDBTableBody>
+                                        {
+                                            this.state.listaProdutos.map(
+                                                function (p) {
+                                                    return (
+                                                        <tr key={p.idProduto}>
+                                                            <td>{p.idProduto}</td>
+                                                            <td>{p.nomeProduto}</td>
+                                                            <td>{p.idCategoriaNavigation.nomeCategoria}</td>
+                                                            <td>
+                                                                <MDBBtn color="primary" size="sm" onClick={() => this.openModalEditarProduto(p)}>
+                                                                    Editar
+                                                                </MDBBtn>
+                                                                <MDBBtn color="danger" size="sm" onClick={() => this.deleteEvento(p.idEvento)}>
+                                                                    Excluir
+                                                                </MDBBtn>
+                                                            </td>
+                                                        </tr>
+                                                    )
+                                                }.bind(this)
+                                            )
+                                        }
+                                    </MDBTableBody>
+                                </MDBTable>
+                            </MDBModalBody>
+                            <MDBModalFooter>
+                                <MDBBtn color="secondary" onClick={this.toggle(3)}>Fechar</MDBBtn>
+                                <MDBBtn color="primary">Salvar</MDBBtn>
+                            </MDBModalFooter>
+                        </MDBModal>
+                    </MDBContainer>
+
+                    {/* Container para cadastrar categorias */}
+                    <MDBContainer>
+                        <MDBModal isOpen={this.state.modal2} toggle={this.toggle(2)} centered>
+                            <MDBModalHeader toggle={this.toggle(2)}>Cadastrar Categoria</MDBModalHeader>
+                            <MDBModalBody>
+                                <form onSubmit={this.postCategoria}>
+                                    <div className="form-group">
+                                        <label htmlFor="example2">Nome da Categoria:</label>
+                                        <input type="text"
+                                            className="form-control form-control-md"
+                                            value={this.state.listaCategorias.nomeCategoria}
+                                            onChange={this.postSetState} />
+                                    </div>
+                                    <MDBBtn color="primary" type="submit">Salvar</MDBBtn>
+                                    {
+                                        this.state.erroMsg &&
+                                        <MDBAlert color="danger" >
+                                            {this.state.erroMsg}
+                                        </MDBAlert>
+                                    }
+                                    {
+                                        this.state.sucessMsg &&
+                                        <MDBAlert color="sucess" >
+                                            {this.state.sucessMsg}
+                                        </MDBAlert>
+                                    }
+                                </form>
+                            </MDBModalBody>
+                            <MDBModalFooter>
+                                <MDBBtn color="secondary" onClick={this.toggle(2)}>Fechar</MDBBtn>
+                            </MDBModalFooter>
+                        </MDBModal>
+                    </MDBContainer>
                 </main>
                 <Footer></Footer>
             </div >
         );
     }
 }
-
-
 
 export default Perfiladm;
--- Frontend/src/pages/Perfilusuario/perfilusuario.js
+++ Frontend/src/pages/Perfilusuario/perfilusuario.js
@@ -2,65 +2,146 @@ import React, { Component } from 'react';
 import avatar from '../../assets/imagens/avatar.png';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
-
+import api from '../../services/api';
 
 class Perfilusuario extends Component {
-    constructor(){
-        super()
+    constructor(props) {
+        super(props)
         this.state = {
+            usuario: {},
 
-
+            isEdit: true
         }
     }
 
-    render() {
+    componentDidMount() {
+        this.getUsuario();
+    }
 
+    getUsuario = () => {
+        api.get('/usuario/1').then(response => {
+            if (response.status === 200) {
+                this.setState({ usuario: response.data })
+            }
+        })
+    }
+
+    // alterarStateUsuario = event => {
+    //     const chave = event.target.name;
+    //     this.setState({
+    //         usuario: {
+    //             chave: event.target.value
+    //         }
+    //     });
+    // }
+    
+    alterarStateUsuario = event => {
+        this.setState({
+            usuario : {
+                ...this.state.usuario, [event.target.name] : event.target.value
+            }
+        });
+    }
+
+    // updateSetState = (input) =>{
+    //     this.setState({
+    //         updateUsuario : {
+    //             ...this.state.updateUsuario, [input.target.name] : input.target.value
+    //         }
+    //     })
+    // }
+
+    updateUsuario = event =>{
+        event.preventDefault();
+        const { senha, email } = this.state.usuario;
+
+        if(senha.length >0 && email.length >0){
+            api.put('/usuario/1', { 
+                senha: senha,
+                email: email
+            }).then(response => {
+                if(response.status === 200) {
+                    console.log('Deu certo');
+                }else {
+                    console.log('Deu ERRADO');
+                }
+            }).catch(error => {
+                console.log(error);
+            });
+        }
+    }
+
+
+    habilitaInput = () => {
+        this.setState({
+            isEdit: false
+        })
+    }
+
+    render() {
         return (
             <div>
                 <Header></Header>
                 <main>
-                    <div class="container">
-                        <div class="cont_branco">
+                    <div className="container">
+                        <div className="cont_branco">
                             <h2>MEU PERFIL</h2>
-                            <hr/>
-                            <div class="container_perfil">
-                                <div class="imgperfil">
+                            <hr />
+                            <div className="container_perfil">
+                                <div className="imgperfil">
                                     <img src={avatar} alt="Imagem de perfil do usuário" />
                                 </div>
-                                <div class="form_perfil">
-                                    <form method="POST" id="form_perfil">
-                                        <label>
-                                            Nome completo
-                                            <input type="text" placeholder="Digite seu nome de usuário..." name="nome"
-                                                aria-label="Nome completo do usuário" required value="Fulano da Silva" />
-                                        </label>
-                                        <label>
-                                            CPF/CNPJ
-                                            <input type="text" placeholder="Digite seu cpf e cnpj..." name="cpf_cnpj"
-                                                aria-label="CPF/CNPJ do usuário" required value="xxx.xxx.xxx-xx" />
-                                        </label>
-                                        <label>
-                                            E-mail
-                                            <input type="text" placeholder="Digite seu email..." name="email"
-                                                aria-label="Email do usuário" required value="fulanosilva@gmail.com" />
-                                        </label>
-                                        <label>
-                                            Usuário
-                                            <input type="text" placeholder="Digite seu nome de usuário..." name="usuario"
-                                                aria-label="Nome de usúario" required value="fulanosilva112" />
-                                        </label>
-                                        <label>
-                                            Senha
-                                            <input type="password" placeholder="Digite sua senha..." name="senha"
-                                                aria-label="Digitar sua senha" required value="••••••••••••" />
-                                        </label>
-                                        <label>
-                                            <div class="btnperfil">
-                                                <button class="btn_perfil" type="submit">Editar</button>
+                                <div className="form_perfil">
+                                    <form onSubmit={this.updateUsuario} id="form_perfil">
 
-                                                <button class="btn_perfil" type="submit">Salvar</button>
+                                        <div>
+                                            <label>
+                                                Nome completo
+                                            <input type="text" 
+                                                placeholder="Digite seu nome de usuário..." 
+                                                name="Nome" 
+                                                value={this.state.usuario.nome}
+                                                disabled
+                                                />
+                                            </label>
+                                            <label>
+                                                CPF/CNPJ
+                                            <input type="text" 
+                                                placeholder="Digite seu cpf e cnpj..." 
+                                                name="Identificador" 
+                                                value={this.state.usuario.identificador}
+                                                disabled
+                                                />
+                                            </label>
+                                            <label>
+                                                E-mail
+                                            <input type="text" 
+                                                placeholder="Digite seu email..." 
+                                                name="Email" 
+                                                value={this.state.usuario.email}
+                                                onChange={this.alterarStateUsuario}
+                                                disabled={this.state.isEdit}
+                                                    />
+                                            </label>
+                                            <label>
+                                                Senha
+                                            <input type="text" 
+                                                placeholder="Digite sua senha..." 
+                                                name="Senha" 
+                                                value={this.state.usuario.senha}
+                                                onChange={this.alterarStateUsuario}
+                                                disabled={this.state.isEdit}
+                                                />
+                                            </label>
+                                        </div>
+                            
+                                        <label>
+                                            <div className="btnperfil">
+                                                <button className="btn_perfil" type="button" onClick={this.habilitaInput} >Editar </button>
+                                                <button className="btn_perfil" type="submit">Salvar</button>
                                             </div>
                                         </label>
+
                                     </form>
                                 </div>
                             </div>
@@ -72,5 +153,4 @@ class Perfilusuario extends Component {
         );
     }
 }
-
 export default Perfilusuario;
