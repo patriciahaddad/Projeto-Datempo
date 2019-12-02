f6fd3fa8bae1a1fadc06f7b02de3ed2c3889849f
nilson-coder <55153255+nilson-coder@users.noreply.github.com>
2019-12-01 21:56:25 -0300

    perfil e minhasOferta

--- Frontend/src/pages/MinhasOfertas/minhasofertas.js
+++ Frontend/src/pages/MinhasOfertas/minhasofertas.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from './node_modules/react';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 import ImagemCard from '../../assets/imagens/arroz.png';
