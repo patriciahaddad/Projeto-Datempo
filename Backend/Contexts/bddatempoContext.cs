067a78291c2c6bd20bc3d980c907bafbe3619fec
brubribeiro <bru.barretoribeiro@gmail.com>
2019-12-13 11:07:13 -0300

    Resolvendo conflitos

--- Backend/Contexts/bddatempoContext.cs
+++ Backend/Contexts/bddatempoContext.cs
@@ -27,7 +27,8 @@ namespace Backend.Domains
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlServer("Server=DESKTOP-GAT13IM\\SQLEXPRESS; Database=bddatempo; User Id=sa; Password=132");
+                //DESKTOP-JHOHBO4\\SQLEXPRESS - BRUNA    
+                optionsBuilder.UseSqlServer("Server=DESKTOP-JHOHBO4\\SQLEXPRESS; Database=bddatempo; User Id=sa; Password=132");
             }
         }
 
